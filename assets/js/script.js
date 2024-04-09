81cb809a51742f80b99a0650fd852e6de614ace8
rinklepoonia <rinklepoonia889@gmail.com>
2024-04-09 11:32:46 +0530

    manage z-index in FAQ section

--- assets/js/script.js
+++ assets/js/script.js
@@ -76,3 +76,18 @@ $('.team_slider').slick({
         // instead of a settings object
     ]
 });
+
+// back-to-top
+const topBtn = document.querySelector(".topBtn");
+const show = document.querySelector(".show");
+
+window.addEventListener("scroll", function () {
+    if (window.scrollY > 500) {
+        topBtn.classList.add("show");
+    } else {
+        topBtn.classList.remove("show");
+    }
+})
+topBtn.addEventListener("click", function () {
+    document.documentElement.scrollTop = 0;
+});
